1309b8e473979540c472fc91288898b7f893a504
Owen Prendergast <owenprendergast8@gmail.com>
2024-10-23 09:36:29 -0400

    fix clerk signup

--- src/components/invite/sign_up.tsx
+++ src/components/invite/sign_up.tsx
@@ -1,98 +1,96 @@
 "use client";
 import React, { useState, useEffect } from "react";
-import { useSearchParams } from 'next/navigation'
-import { User } from "@prisma/client";
-import { emptyUser } from "@/lib/util/types";
-
+import { useSearchParams } from "next/navigation";
+// import { User } from "@prisma/client";
+// import { emptyUser } from "@/lib/util/types";
+import { useSignUp } from "@clerk/nextjs";
+import { SignUpResource } from "@clerk/types";
 
 export default function SignUp() {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [confirm, setConfirm] = useState<string>("");
+  const { isLoaded, signUp } = useSignUp();
 
+  // console.log("isLoaded:", isLoaded);
+  // console.log("signUp:", signUp);
 
-  
-  const inviteToken = useSearchParams().get('__clerk_ticket')
+  const inviteToken = useSearchParams().get("__clerk_ticket");
+
   useEffect(() => {
-    console.log("HELLLOOOOOOOOO")
-    console.log("my invite token: " + inviteToken)
-  })
+    const clerkSignup = async () => {
+      if (isLoaded) {
+        console.log("signing up");
+        const signUpAttempt = await (signUp as SignUpResource).create({
+          strategy: "ticket",
+          ticket: inviteToken || "",
+        });
 
-  //User clicks on link to invitation -> Clerk 
+        console.log(signUpAttempt);
+        setEmail(signUpAttempt.emailAddress || "");
+      }
+    };
+
+    clerkSignup();
+  });
+
+  //User clicks on link to invitation -> Clerk
   //
 
-  const on_submit = () => {
-    
+  const on_submit = async () => {
     if (password != confirm) {
-        
-        alert("Passwords do not match!");
+      alert("Passwords do not match!");
     } else {
-        console.log("Passwords match. Form submitted")
+      console.log("Passwords match. Form submitted");
     }
-    
-    // const signUpAttempt = await signUp.create({
-    //     strategy: 'ticket',
-    //     ticket: inviteToken,
-    //     firstName,  do we actually need these?
-    //     lastName, dont know this
-    //     password,
-    //   })
 
     // if (signUpAttempt.status === "complete")
-        //  get the public metadata
-        // let newUser = emptyUser
-        // newUser.email = email; etc
+    //  get the public metadata
+    // let newUser = emptyUser
+    // newUser.email = email; etc
 
-        // createUser(newUser)
+    // createUser(newUser)
   };
 
   if (!inviteToken) {
-    return (<p>no invite found</p>)
-  }
-  return (
-    <div>
-      <p>Email: </p>
-      <input
-        type="text"
-        name="Email"
-        className="text-black"
-        onChange={(event) => {
-          setEmail(event.target.value);
-        }}
-      />
-      <p>Password: </p>
-      <input
-        type="text"
-        name="email"
-        className="text-black"
-        onChange={(event) => {
-          setPassword(event.target.value);
-        }}
-      />
-      <p>Password: </p>
-      <input
-        type="text"
-        name="email"
-        className="text-black"
-        onChange={(event) => {
+    return <p>no invite found</p>;
+  } else if (email == "") {
+    return <p>loading</p>;
+  } else {
+    return (
+      <div>
+        <p>Email: </p>
+        <p>{email}</p>
+        <p>Password: </p>
+        <input
+          type="text"
+          name="email"
+          className="text-black"
+          onChange={(event) => {
+            setPassword(event.target.value);
+          }}
+        />
+        <p>Password: </p>
+        <input
+          type="text"
+          name="email"
+          className="text-black"
+          onChange={(event) => {
             setConfirm(event.target.value);
-        }}
-      />
-      <br></br>
-      <button onClick = {on_submit}> Sign-up</button>
-    </div>
-  );
+          }}
+        />
+        <br></br>
+        <button onClick={on_submit}> Sign-up</button>
+      </div>
+    );
+  }
 }
-
-
 
 // export const User: Omit<User, "id" | "createdAt" | "updatedAt"> = {
 //     name: "Bob",
 //     email: "bob@gmail.com",
 //     role: "Admin",
 //   };
-
-
 
 /*
 
