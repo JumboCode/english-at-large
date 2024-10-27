893ec8b91a7786b8718c2daff536b095e600446d
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2024-10-27 00:31:46 -0400

    Squashed commit of the following:
    commit 2b6ba774007c2a4c53e38804cd27ae260cbb1eea
    Author: LordofMankid <58374778+LordofMankid@users.noreply.github.com>
    Date:   Wed Oct 23 19:10:54 2024 -0400
    
        added types to pass deployment linter
    
    commit 5853a9a0e215f17dd4315be6623b9a0742d06cea
    Author: LordofMankid <58374778+LordofMankid@users.noreply.github.com>
    Date:   Wed Oct 23 19:05:09 2024 -0400
    
        removed stuff
    
    commit 548185927f253e6febd492c6e2f16f313d401d06
    Author: LordofMankid <58374778+LordofMankid@users.noreply.github.com>
    Date:   Tue Oct 22 00:05:00 2024 -0400
    
        layout changes
    
    commit 1e105d8428e19ea156abb5743d8b177c25cbb634
    Author: Jet4stream <158139019+Jet4stream@users.noreply.github.com>
    Date:   Sun Oct 20 23:43:30 2024 -0400
    
        Knumata5127/eng 18 crud operations books (#14)
    
        * Created Jet.txt
    
        * Update Jet.txt
    
        * jet doesn't shower
    
        * Implemented Update
    
        Update implemented
    
        * Update UpdateBookButton.tsx
    
        * Added delete in CRUD and button
    
        * Changes
    
        changes
    
        * Updated Get
    
        * Finished with the getOne function
    
        * Added Getall to Controllers
    
        * Added the GetAll button
    
        * some cleanup
    
        * PR Changes
    
        Changed per the PR
    
        * controller cleanup
    
        ---------
    
        Co-authored-by: keiji-numata <147024363+keiji-numata@users.noreply.github.com>
        Co-authored-by: LordofMankid <58374778+LordofMankid@users.noreply.github.com>

---
+++ src/components/testing/GetOneBookButton.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { useState } from "react";
+import { getOneBook } from "@/lib/api/books"; // Adjust the path as necessary
+// import { emptyBook } from "@/lib/util/types";
+
+const GetOneBookButton = () => {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleGetOneBook = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      const book = await getOneBook(0);
+      if (book) alert("Book getOne successfully!");
+      else throw error;
+    } catch (err) {
+      setError("Failed to fetch book");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <button onClick={() => handleGetOneBook()} disabled={loading}>
+        {loading ? "Updating..." : "getOne Book"}
+      </button>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+    </div>
+  );
+};
+
+export default GetOneBookButton;
