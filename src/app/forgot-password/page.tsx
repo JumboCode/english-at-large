8c69f30e94b098bbd2da408e7a908de8216500c8
natphua <108436412+natphua@users.noreply.github.com>
2024-10-28 17:04:18 -0400

    Nataliephua10/eng 30 login forgot password (#18)
    * created pages and basic skeleton
    
    * made forgot password page
    
    * small font changes
    
    * testing procedures
    
    * final changes
    
    * removed need for scrolling
    
    * finished responsiveness
    
    * Squashed commit of the following:
    
    commit 2b6ba774007c2a4c53e38804cd27ae260cbb1eea
    Author: LordofMankid <58374778+LordofMankid@users.noreply.github.com>
    Date:   Wed Oct 23 19:10:54 2024 -0400
    
        added types to pass deployment linter
    
    commit 5853a9a0e215f17dd4315be6623b9a0742d06cea
    Author: LordofMankid <58374778+LordofMankid@users.noreply.github.com>
    Date:   Wed Oct 23 19:05:09 2024 -0400
    
        removed stuff
    
    * added responsiveness to image
    
    * fixed image stretch
    
    * made txt smaller
    
    * margin
    
    * fixed hydration issues
    
    * changing diff
    
    ---------
    
    Co-authored-by: Sela Alonso <selaalonso@Selas-MacBook-Pro.local>
    Co-authored-by: LordofMankid <58374778+LordofMankid@users.noreply.github.com>

---
+++ src/app/forgot-password/page.tsx
@@ -0,0 +1,58 @@
+"use client";
+import ForgotPasswordForm from "@/components/forgot-password/ForgotPasswordForm";
+import books from "@/images/books.jpg";
+import CommonButton from "@/components/common/button/CommonButton";
+import Image from "next/image";
+
+const ForgotPassword = () => {
+  const handleReset = () => {
+    return;
+  };
+
+  return (
+    <div className="font-family-name:var(--font-geist-sans)] grid grid-cols-7 items-center">
+      <div className="hidden md:block container relative h-full min-h-screen col-span-4">
+        <Image
+          aria-hidden
+          src={books}
+          alt="books"
+          layout="fill"
+          className="object-cover"
+        />
+      </div>
+
+      <div className="container mx-auto px-20 object-fill col-span-7 md:col-span-3 mt-5 ">
+        <div className="text-2xl font-bold flex flex-col-3">
+          <h1>Forgot password?</h1>
+        </div>
+
+        <div className="text-m pb-10 text-zinc-500">
+          <p>
+            {" "}
+            All good. Enter your account&apos;s email and we&apos;ll send you a
+            link to reset your password.{" "}
+          </p>
+        </div>
+        <ForgotPasswordForm />
+
+        <div>
+          <CommonButton
+            label="Send reset link"
+            onClick={handleReset}
+            altTextStyle="text-white"
+            altStyle="bg-dark-blue mt-10 w-full"
+          />
+          <a href="/login">
+            <CommonButton
+              label="Back to login"
+              altTextStyle="text-dark-blue"
+              altStyle="bg-white outline-dark-blue mt-3 w-full"
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ForgotPassword;
