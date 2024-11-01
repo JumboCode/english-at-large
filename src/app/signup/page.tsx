db5fd89a1dcf1d7a02d90859a1b9d1cb7c06f2d4
keiji-numata <147024363+keiji-numata@users.noreply.github.com>
2024-11-01 14:41:18 -0400

    Password hide
    Password hide implemented

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -16,6 +16,17 @@ const SignUp = () => {
   const [passwordsMatch, setPasswordsMatch] = useState<boolean>(true);
   const [isFormValid, setIsFormValid] = useState<boolean>(false);
 
+  const [isPasswordVisible, setIsPasswordVisible] = useState<boolean>(false);
+  const [isConfirmPasswordVisible, setIsConfirmPasswordVisible] = useState<boolean>(false);
+
+  const togglePasswordVisibility = () => {
+    setIsPasswordVisible(!isPasswordVisible);
+  };
+
+  const toggleConfirmPasswordVisibility = () => {
+    setIsConfirmPasswordVisible(!isConfirmPasswordVisible);
+  };
+
   // check if password & confirm password input match
   useEffect(() => {
     setPasswordsMatch(password === confirmPassword);
@@ -80,28 +91,29 @@ const SignUp = () => {
               Please enter a valid email address with an @ symbol.
             </span>
           </div>
-          <div className="mb-6">
-            <label
+          <div className="mb-6 relative">
+          <label
               htmlFor="password"
               className="block mb-2 text-lg font-[700] font-medium text-gray-900 dark:text-white"
             >
               Create Password
             </label>
             <input
-              type="password"
+              type={isPasswordVisible ? "text" : "password"}
               id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500 peer"
-              placeholder=" "
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white"
               required
               onChange={(e) => setNewPassword(e.target.value)}
               pattern="^(?=.*[a-z])(?=.*[A-Z]).{8,}$"
             />
-            <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
-              Password must be at least 8 characters long with an uppercase and
-              a lowercase letter.
+            <span
+              className="absolute right-3 top-10 cursor-pointer"
+              onClick={togglePasswordVisibility}
+            >
+              <InfoIcon name={isPasswordVisible ? "eye-off" : "eye"} />
             </span>
           </div>
-          <div className="mb-6">
+          <div className="mb-6 relative">
             <label
               htmlFor="confirm_password"
               className="block mb-2 text-lg font-[700] font-medium text-gray-900 dark:text-white"
@@ -109,22 +121,25 @@ const SignUp = () => {
               Confirm Password
             </label>
             <input
-              type="password"
+              type={isConfirmPasswordVisible ? "text" : "password"}
               id="confirm_password"
               className={`bg-gray-50 border ${
-                confirmPassword && !passwordsMatch
-                  ? "border-red-500"
-                  : "border-gray-300"
-              } text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 peer`}
-              placeholder=" "
+                confirmPassword && !passwordsMatch ? "border-red-500" : "border-gray-300"
+              } text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:text-white`}
               required
               onChange={(e) => setConfirmPassword(e.target.value)}
             />
             {confirmPassword && !passwordsMatch && (
-              <span className="mt-2 text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+              <span className="mt-2 text-sm text-red-500">
                 Passwords do not match.
               </span>
             )}
+            <span
+              className="absolute right-3 top-10 cursor-pointer"
+              onClick={toggleConfirmPasswordVisibility}
+            >
+              <InfoIcon name={isConfirmPasswordVisible ? "eye-off" : "eye"} />
+            </span>
           </div>
           <button
             type="submit"
