bad14747fdcad09f46e6ec5843ddcf8a1c2dc95b
21hwang <130326162+21hwang@users.noreply.github.com>
2024-11-11 21:19:15 +0000

    21hwang/eng 27 edit book information form (#24)
    * edit form minus tags
    
    * fixed skills and close on save
    
    * formatting
    
    * type fixes
    
    * edit book & remove book
    
    * deleted edit form component
    
    * formatting & linting
    
    * fix
    
    * on save
    
    * bookId = 0 and form reload edge case
    
    ---------
    
    Co-authored-by: LordofMankid <58374778+LordofMankid@users.noreply.github.com>

--- src/components/details.tsx
+++ src/components/details.tsx
@@ -6,19 +6,30 @@ interface DetailProps {
   publisher: string;
   releaseDate: string | null;
   copies: number;
+  lineSpacing?: string;
+  verticalSpacing?: string;
+  fontSize?: string;
 }
 
 const BookDetail = (props: DetailProps) => {
-  const { isbn, publisher, releaseDate, copies } = props;
+  const {
+    isbn,
+    publisher,
+    releaseDate,
+    copies,
+    lineSpacing,
+    verticalSpacing,
+    fontSize,
+  } = props;
   return (
     <div className="flex row-span-2 my-5">
-      <div>
+      <div className={`font-medium ${lineSpacing} ${fontSize}`}>
         <div className="relative text-gray-500 mr-4">ISBN</div>
         <div className="relative text-gray-500 mr-4">Publisher</div>
         <div className="relative text-gray-500 mr-4">Release</div>
         <div className="relative text-gray-500 mr-4">Copies</div>
       </div>
-      <div>
+      <div className={`${lineSpacing} ${verticalSpacing} ${fontSize}`}>
         <div className="relative">{isbn}</div>
         <div className="relative">{publisher}</div>
         <div className="relative">{releaseDate}</div>
