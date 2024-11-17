4c6ca2e4cdf0fc80a23dd20f27b964956fe1a2cf
keiji-numata <147024363+keiji-numata@users.noreply.github.com>
2024-11-17 11:37:48 -0500

    Login Error
    Fixed Login error

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,31 +3,22 @@ import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuth } from "@clerk/nextjs";
 
-
 export default function Home() {
   const router = useRouter();
   const { userId, isLoaded } = useAuth();
 
-  // useEffect(() => {
-  //   console.log("Auth State:", { isLoaded, userId });  // Debug log
+  useEffect(() => {
+    if (isLoaded) {
+      if (!userId) {
+        router.replace("/login");
+      } else {
+        router.replace("/dashboard/books");
+      }
+    }
+  }, [userId, isLoaded, router]);
 
-  //   if (isLoaded) {
-  //     if (!userId) {
-  //       console.log("No user ID, redirecting to login");  // Debug log
-  //       router.replace("/login");
-  //     } else {
-  //       console.log("User ID found, redirecting to dashboard");  // Debug log
-  //       window.location.href = "/dashboard/books";  // Force redirect
-  //     }
-  //   }
-  // }, [userId, isLoaded, router]);
-
-  
-  // //router.push("/login");
-  // if (!isLoaded) {
-  //   return <div>Loading...</div>;
-  // }
-  window.location.href = "/dashboard/books";
-
+  if (!isLoaded) {
+    return <div>Loading...</div>;
+  }
   return <div></div>;
 }
