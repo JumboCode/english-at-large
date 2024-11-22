9ed057e31e6d65124dd68e4aca68b6be0cc3ff1f
ava-sim <146499504+ava-sim@users.noreply.github.com>
2024-11-21 20:37:59 -0500

    Request functionality (#29)
    * first version with initial modal, confirmation modal not made yet
    
    * working on backend for requests
    
    * Request functionality
    
    * Request Functionality
    
    * ready for PR
    
    * renaming weirdness
    
    * rename weirdness 2
    
    * post merge fixes
    
    * schema changes, added request
    
    ---------
    
    Co-authored-by: gdozortsev <81634840+gdozortsev@users.noreply.github.com>
    Co-authored-by: LordofMankid <58374778+LordofMankid@users.noreply.github.com>

--- src/components/RemoveModal.tsx
+++ src/components/RemoveModal.tsx
@@ -3,7 +3,7 @@ import { deleteBook } from "@/lib/api/books";
 import CommonButton from "./common/button/CommonButton";
 import { Book } from "@prisma/client";
 import { useRouter } from "next/navigation";
-import BookDetail from "./details";
+import BookDetail from "./Details";
 
 interface RemoveModalProps {
   setShowRemoveModal: (arg0: boolean) => void;
