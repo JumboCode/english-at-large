bc32b82b18a88ace1f6b500ec4e7409ec62ba99b
Jet4stream <yotsuuye@gmail.com>
2024-11-25 11:54:45 -0500

    Updated the loan page

--- src/app/dashboard/loans/page.tsx
+++ src/app/dashboard/loans/page.tsx
@@ -1,21 +1,37 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import SendInvite from "../../../components/manage/sendInvite";
+// import SendInvite from "../../../components/manage/sendInvite";
 import SearchBar from "@/components/SearchBar";
 import CommonButton from "@/components/common/button/CommonButton";
-import { User, BookRequest } from "@prisma/client";
-import { getAllUsers, getOneUser } from "@/lib/api/users";
+import { User, BookRequest, BookStatus } from "@prisma/client";
+// import { getAllUsers, getOneUser } from "@/lib/api/users";
 import CommonDropdown from "@/components/common/forms/Dropdown";
-import PendingChip from "@/assets/icons/pending_chip";
-import { deleteUser } from "@/lib/api/users";
-import Link from "next/link";
+// import PendingChip from "@/assets/icons/pending_chip";
+// import { deleteUser } from "@/lib/api/users";
+// import Link from "next/link";
 import { dateToTimeString } from "@/lib/util/utilFunctions";
 import { getRequests, updateRequest } from "@/lib/api/requests";
 
-const loans = () => {
+const Loans = () => {
   // const [users, setUsers] = useState<User[]>([]);
-  const [popupOpen, setPopupOpen] = useState<boolean>(false);
+  // const [popupOpen, setPopupOpen] = useState<boolean>(false);
   const [requests, setRequests] = useState<BookRequest[]>([])
+  const [oneRequest, setOneRequest] = useState<BookRequest>();
+  const [selectedValue, setSelectedValue] = useState<string>("");
+  
+
+  // const updateReq = async (req: BookRequest) => {
+  //   await updateRequest(req)
+  // }
+
+  const updateReq = async (req: BookRequest) => {
+    await updateRequest(req)
+    setOneRequest(req)
+  }
+
+  // const toggleSelectedValue = () => {
+  //   setSelectedValue(!selectedValue);
+  // }
 
   useEffect(() => {
     // const getUsers = async () => {
@@ -24,31 +40,39 @@ const loans = () => {
     // };
 
     // getUsers();
-
+  
     const getReqs = async () => {
       const allRequests = await getRequests();
       setRequests(allRequests ?? [])
     }
 
-    getReqs();
-  }, []);
 
-  const getUser = async (id: string) => {
-    const user = await getOneUser(id);
-    return user;
-  }
+    getReqs();
+
+  }, [oneRequest]);
+
+  useEffect(() => {
+    // if (selectedValue == "Pickup") {
+    //   updateReq({...request, status : BookStatus.Returned})
+    // }
+  }, [selectedValue])
+
+  // const getUser = async (id: string) => {
+  //   const user = await getOneUser(id);
+  //   return user;
+  // }
   
   return (
     <div className="bg-white">
       <h1 className="bg-white text-black px-16 pt-12 font-bold text-3xl font-[family-name:var(--font-rubik)]">
         Manage loans
       </h1>
-      <SearchBar
-        button={
-          <CommonDropdown items={["Request Date", "Return Date", "Pick-up", "Borrowed"]} />
-        }
+      {/* <SearchBar
+        // button={
+        //   <CommonDropdown items={["Request Date", "Return Date", "Pick-up", "Borrowed"]}  />
+        // }
         placeholderText="Search by name or email"
-      />
+      /> */}
       <div className="px-16">
         <table className="table-auto bg-white w-full font-family-name:var(--font-geist-sans)]">
           <thead>
@@ -72,7 +96,7 @@ const loans = () => {
             </tr>
           </thead>
           <tbody className="divide-y divide-solid">
-            {requests.map((request, index) => (
+            {requests.filter((request) => {return request.status !== BookStatus.Returned}).map((request, index) => (
               <tr key={index} className="bg-white h-16">
                 <td className="flex flex-col">
                   <p className="text-black font-semibold">{request.userId}</p>
@@ -80,7 +104,7 @@ const loans = () => {
                     href={"mailto:" + user.email}
                     className="text-text-default-secondary underline max-w-max"
                   > */}
-                    {request.userId}
+                    {/* {getUser(request.userId)} */}
                   {/* </Link> */}
                 </td>
                 <td className=" text-black">
@@ -88,7 +112,7 @@ const loans = () => {
                     {request.bookId}
                   </div>
                 </td>
-                <td>{user.pending ? <PendingChip /> : null}</td>
+                {/* <td>{re ? <PendingChip /> : null}</td> */}
                     
                 <td className="text-black">
                   {dateToTimeString(request.requestedOn)}
@@ -97,17 +121,37 @@ const loans = () => {
                 <td className="text-black">
                   {dateToTimeString(request.returnedBy)}
                 </td>
+
+                <td className="text-black">
+                  {/* {setSelectedValue(request.status)} */}
+                  {selectedValue == "Pickup" ? 
+                    (<CommonDropdown items={["Borrowed"]} 
+                      defaultValue="Pickup"
+                      // hideSelectedOption={true}
+                      selectedValue={selectedValue}
+                      setSelectedValue={setSelectedValue}
+                      /> 
+            
+                    )
+                    : 
+                    (<CommonDropdown items={["Pickup"]} 
+                      defaultValue="Borrowed"
+                      // hideSelectedOption={true} 
+                      selectedValue={selectedValue}
+                      setSelectedValue={setSelectedValue}
+                      />
+                    )}
+                </td>
                     
                 <td>
-                  <div className="flex justify-end items-center">
+                  <div className="flex justify-start items-center">
                     <CommonButton
                       label="Mark as Returned"
                       onClick={() => {
-                        
-                        updateRequest(request)
+                        updateReq({...request, status : BookStatus.Returned})
                       }}
-                      altTextStyle="text-white"
-                      altStyle="bg-blue"
+                      altTextStyle="text-blue"
+                      altStyle="bg-white"
                     />
                   </div>
                 </td>
@@ -120,4 +164,4 @@ const loans = () => {
   )
 }
 
-export default loans
+export default Loans
