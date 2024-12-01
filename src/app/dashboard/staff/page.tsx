80f4656654cb463947daee7f993047f25cdfcfa6
javier-la200426 <javier.la200426@gmail.com>
2024-12-01 12:31:49 -0500

    formatting prettier

--- src/app/dashboard/staff/page.tsx
+++ src/app/dashboard/staff/page.tsx
@@ -13,29 +13,17 @@ import { dateToTimeString } from "@/lib/util/utilFunctions";
 import useCurrentUser from "@/lib/hooks/useCurrentUser";
 import { redirect } from "next/navigation";
 
-
-
 export default function Manage() {
-
-  const user = useCurrentUser(); 
+  const user = useCurrentUser();
 
   useEffect(() => {
     if (user?.role != "Admin" && user?.role != undefined) {
-      console.log("REDIRECTING")
       redirect("/dashboard");
     }
   }, [user]);
 
-
-
- 
-  
-  
-  
   const [users, setUsers] = useState<User[]>([]);
   const [popupOpen, setPopupOpen] = useState<boolean>(false);
-
-  
 
   useEffect(() => {
     const getUsers = async () => {
@@ -48,87 +36,87 @@ export default function Manage() {
 
   return (
     <div>
-    {user?.role === "Admin" ? (
-    <div className="bg-white">
-      <h1 className="bg-white text-black px-16 pt-12 font-bold text-3xl font-[family-name:var(--font-rubik)]">
-        Users
-      </h1>
-      <SearchBar
-        button={
-          <CommonDropdown items={["All", "Tutors", "Admins", "Pending"]} />
-        }
-        button2={
-          <CommonButton
-            label="Invite User"
-            onClick={() => {
-              setPopupOpen(true);
-            }}
-            altTextStyle="text-white"
-            altStyle="bg-dark-blue"
+      {user?.role === "Admin" ? (
+        <div className="bg-white">
+          <h1 className="bg-white text-black px-16 pt-12 font-bold text-3xl font-[family-name:var(--font-rubik)]">
+            Users
+          </h1>
+          <SearchBar
+            button={
+              <CommonDropdown items={["All", "Tutors", "Admins", "Pending"]} />
+            }
+            button2={
+              <CommonButton
+                label="Invite User"
+                onClick={() => {
+                  setPopupOpen(true);
+                }}
+                altTextStyle="text-white"
+                altStyle="bg-dark-blue"
+              />
+            }
+            placeholderText="Search by name or email"
           />
-        }
-        placeholderText="Search by name or email"
-      />
-      <div className="px-16">
-        <table className="table-auto bg-white w-full font-family-name:var(--font-geist-sans)]">
-          <thead>
-            <tr className="bg-gray-100">
-              <th className="w-1/2 text-left text-text-default-secondary">
-                Name
-              </th>
-              <th className="w-[4.166666%] text-left text-text-default-secondary">
-                Role
-              </th>
-              <th className="w-[12.499999%] text-left"> </th>
-              <th className="w-1/6 text-left text-text-default-secondary">
-                Date Invited
-              </th>
-              <th className="w-1/6" />
-            </tr>
-          </thead>
-          <tbody className="divide-y divide-solid">
-            {users.map((user, index) => (
-              <tr key={index} className="bg-white h-16">
-                <td className="flex flex-col">
-                  <p className="text-black font-semibold">{user.name}</p>
-                  <Link
-                    href={"mailto:" + user.email}
-                    className="text-text-default-secondary underline max-w-max"
-                  >
-                    {user.email}
-                  </Link>
-                </td>
-                <td className=" text-black">
-                  <div className="flex justify-between min-w-max max-w-[50%]">
-                    {user.role}
-                  </div>
-                </td>
-                <td>{user.pending ? <PendingChip /> : null}</td>
+          <div className="px-16">
+            <table className="table-auto bg-white w-full font-family-name:var(--font-geist-sans)]">
+              <thead>
+                <tr className="bg-gray-100">
+                  <th className="w-1/2 text-left text-text-default-secondary">
+                    Name
+                  </th>
+                  <th className="w-[4.166666%] text-left text-text-default-secondary">
+                    Role
+                  </th>
+                  <th className="w-[12.499999%] text-left"> </th>
+                  <th className="w-1/6 text-left text-text-default-secondary">
+                    Date Invited
+                  </th>
+                  <th className="w-1/6" />
+                </tr>
+              </thead>
+              <tbody className="divide-y divide-solid">
+                {users.map((user, index) => (
+                  <tr key={index} className="bg-white h-16">
+                    <td className="flex flex-col">
+                      <p className="text-black font-semibold">{user.name}</p>
+                      <Link
+                        href={"mailto:" + user.email}
+                        className="text-text-default-secondary underline max-w-max"
+                      >
+                        {user.email}
+                      </Link>
+                    </td>
+                    <td className=" text-black">
+                      <div className="flex justify-between min-w-max max-w-[50%]">
+                        {user.role}
+                      </div>
+                    </td>
+                    <td>{user.pending ? <PendingChip /> : null}</td>
 
-                <td className="text-black">
-                  {dateToTimeString(user.createdAt)}
-                </td>
-                <td>
-                  <div className="flex justify-end items-center">
-                    <CommonButton
-                      label="Remove User"
-                      onClick={() => {
-                        deleteUser(user.id);
-                        location.reload(); // next js router.refresh() was not working
-                      }}
-                      altTextStyle="text-dark-blue"
-                      altStyle="bg-white"
-                    />
-                  </div>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <SendInvite isOpen={popupOpen} exit={() => setPopupOpen(false)} />
+                    <td className="text-black">
+                      {dateToTimeString(user.createdAt)}
+                    </td>
+                    <td>
+                      <div className="flex justify-end items-center">
+                        <CommonButton
+                          label="Remove User"
+                          onClick={() => {
+                            deleteUser(user.id);
+                            location.reload(); // next js router.refresh() was not working
+                          }}
+                          altTextStyle="text-dark-blue"
+                          altStyle="bg-white"
+                        />
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <SendInvite isOpen={popupOpen} exit={() => setPopupOpen(false)} />
+        </div>
+      ) : null}
     </div>
-  ) : (null) }
-  </div>
   );
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -5,10 +5,8 @@ import { NextResponse } from "next/server";
 // Define protected routes
 const isProtectedRoute = createRouteMatcher(["/dashboard(.*)"]);
 
-
 export default clerkMiddleware(async (auth, req) => {
   const { userId } = await auth();
-  
 
   if (isProtectedRoute(req) && !userId) {
     //comment this out if you want to access dashboard without being logged in
