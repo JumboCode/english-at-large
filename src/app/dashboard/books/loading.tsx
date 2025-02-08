58ac69ddda5452ffdf8d022c6b1b2ae22fcd9aa8
Sela Alonso <selaalonso@Selas-MacBook-Pro.local>
2025-02-07 21:40:23 -0500

    styled book loading page

--- src/app/dashboard/books/loading.tsx
+++ src/app/dashboard/books/loading.tsx
@@ -9,12 +9,19 @@
  */
 export default function LoadingSkeleton() {
   return (
-    <div>
-      <div className="animate-pulse space-y-2">
-      <div className="h-6 bg-gray-300 rounded w-3/4"></div>
-      <div className="h-4 bg-gray-300 rounded w-1/2"></div>
-      <div className="h-16 bg-gray-200 rounded"></div>
+    <div className="p-1">
+       <div className="animate-pulse flex flex-row space-x-8 justify-left">
+       <div className="h-[35vh] bg-[#EBEAE7] rounded-xl w-1/3"></div>
+       <div className="flex flex-col w-2/5 justify-center">
+       <div className="h-12 bg-[#EBEAE7] rounded-xl w-full mb-6"></div>
+       <div className="flex flex-row w-full space-x-2">
+       <div className="h-5 bg-[#EBEAE7] rounded-xl w-2/5 mb-2"></div>
+       <div className="h-5 bg-[#EBEAE7] rounded-xl w-2/5 mb-2"></div>
+       </div>
+       <div className="h-5 bg-[#EBEAE7] rounded-xl w-2/5 mb-2"></div>
+       </div>
+     </div>
     </div>
-    </div>
+  
   );
 }
