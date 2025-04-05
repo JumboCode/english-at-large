f1f363a1b2f331997f4786b0ab40ebd5301d8cff
LordofMankid <58374778+LordofMankid@users.noreply.github.com>
2025-04-05 19:15:37 -0400

    added typescript changes

--- src/app/dashboard/books/page.tsx
+++ src/app/dashboard/books/page.tsx
@@ -110,7 +110,10 @@ const BooksPage = () => {
       try {
         // change second arg to alter number of books displayed on 1 page
         // 20 is limit of how many books to load
-        const booksResult = await getAllBooks(currentBookPage, 20);
+        const booksResult = await getAllBooks({
+          page: currentBookPage,
+          limit: 20,
+        });
         if (booksResult) {
           const { books: fetchedBooks, totalPages: fetchedTotalPages } =
             booksResult;
