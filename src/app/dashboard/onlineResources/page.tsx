e5af4c9a309cbc9420726ef6f28bf04b33b2a6c8
Owen Prendergast <owenprendergast8@gmail.com>
2025-04-10 16:29:20 -0400

    remove comments

--- src/app/dashboard/onlineResources/page.tsx
+++ src/app/dashboard/onlineResources/page.tsx
@@ -1,77 +1,10 @@
 "use client";
 import React, { useEffect } from "react";
-import OnlineResourceForm from "@/components/common/forms/OnlineResourceForm";
 import { useState } from "react";
-// import {
-//   OnlineResource,
-//   BookSkills,
-//   BookLevel,
-//   // ResourceFormat,
-//   // ResourceTopic,
-// } from "@prisma/client";
-import SearchBar from "@/components/SearchBar";
-// import FilterPopup from "@/components/common/FilterPopup";
-import CommonButton from "@/components/common/button/CommonButton";
-// import FilterIcon from "@/assets/icons/Filter";
-import AddIcon from "@/assets/icons/Add";
-import useCurrentUser from "@/lib/hooks/useCurrentUser";
-import { usePopup } from "@/lib/context/ConfirmPopupContext";
-import ConfirmationPopup from "@/components/common/message/ConfirmationPopup";
 import DisplayFolder from "@/components/common/DisplayFolder";
 import axios from "axios";
 
-enum formState {
-  FORM_CLOSED,
-  RESOURCE_FORM_OPEN,
-}
-
 const OnlineResourcesPage = () => {
-  const user = useCurrentUser();
-  const [formShown, setFormShown] = useState<formState>(formState.FORM_CLOSED);
-  // const [isFilterOpen, setIsFilterOpen] = useState<boolean>(false);
-
-  // states needed for filter functionality -- don't delete!!
-  // const [format, setFormat] = useState<ResourceFormat>();
-  // const [topic, setTopic] = useState<ResourceTopic>();
-
-  // from books dashboard, delete when filter implemented
-  // const [bookSortBy, setBookSortBy] = useState<string>("By Title");
-
-  const { hidePopup, popupStatus } = usePopup();
-  // const [searchData, setSearchData] = useState("");
-
-  // const toggleFilterPopup = () => {
-  //   setIsFilterOpen(!isFilterOpen);
-  // };
-
-  // const filterResources = useCallback(
-  //   (resource: {
-  //     id: string;
-  //     kind: string;
-  //     mimeType: string;
-  //     name: string;
-  //   }) => {
-  //     return (
-  //       (skills.length === 0 ||
-  //         skills.some((skill) => resource.skills.includes(skill))) &&
-  //       (levels.length === 0 || levels.includes(resource.level))
-  //     );
-  //   },
-  //   [levels, skills]
-  // );
-
-  // const subsetResources = structuredClone<
-  //   {
-  //     id: string;
-  //     kind: string;
-  //     mimeType: string;
-  //     name: string;
-  //   }[]
-  // >(resourceFolders).filter((resource) =>
-  //   resource.name.toLowerCase().includes(searchData)
-  // );
-  // .filter((resource) => filterResources(resource));
-
   const [resourceFolders, setResourceFolders] = useState<
     { name: string; id: string; count: number }[]
   >([
@@ -127,58 +60,10 @@ const OnlineResourcesPage = () => {
     };
 
     fetchSubFolders();
-  }, []);
+  });
 
-  return formShown == formState.RESOURCE_FORM_OPEN ? (
-    <OnlineResourceForm
-      exit={() => setFormShown(formState.FORM_CLOSED)}
-      existingResource={null}
-    />
-  ) : (
+  return (
     <div>
-      <SearchBar
-        setSearchData={(_: string) => {}}
-        button={
-          // <CommonButton
-          //   label="Filter"
-          //   leftIcon={<FilterIcon />}
-          //   onClick={toggleFilterPopup}
-          // />
-          null
-        }
-        button2={
-          user?.role === "Admin" ? (
-            <CommonButton
-              label="Add new"
-              leftIcon={<AddIcon />}
-              onClick={() => setFormShown(formState.RESOURCE_FORM_OPEN)}
-              altTextStyle="text-white"
-              altStyle="bg-dark-blue"
-            />
-          ) : null
-        }
-        placeholderText="Search for resources"
-      />
-      {popupStatus.shown ? (
-        <ConfirmationPopup
-          type={popupStatus.type}
-          action={popupStatus.action}
-          success={popupStatus.success}
-          onDisappear={() => hidePopup()}
-          custom={popupStatus.custom}
-        />
-      ) : null}
-
-      {/* <FilterPopup
-        isOpen={isFilterOpen}
-        toggle={toggleFilterPopup}
-        skills={skills}
-        setSkills={setSkills}
-        levels={levels}
-        setLevels={setLevels}
-        sortBook={bookSortBy}
-        setSortBook={setBookSortBy}
-      /> */}
       <div className="p-4 px-16 bg-white border-t">
         <div className="flex flex-row text-left whitespace-normal">
           <p className="text-sm text-slate-500 mb-6">
