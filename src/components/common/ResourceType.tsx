f1e493109d25fc9486afe01c50540c0316380d92
Owen Prendergast <123135948+owennjpr@users.noreply.github.com>
2025-04-10 18:01:52 -0400

    ENG-97 Online Resources (#80)
    * updated online resources page to match figma
    
    * remove comments
    
    * delete resource logic

--- src/components/common/ResourceType.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-// import ResourceInfo from './ResourceInfo'
-// import { OnlineResource } from "@prisma/client"
-
-function ResourceType() {
-  return <div>ResourceType</div>;
-}
-
-export default ResourceType;
