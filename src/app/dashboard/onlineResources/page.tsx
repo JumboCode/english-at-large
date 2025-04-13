90da1501b4813fbe6138278faac29361f21258ee
Owen Prendergast <owenprendergast8@gmail.com>
2025-04-13 19:28:38 -0400

    made it better

--- src/app/dashboard/onlineResources/page.tsx
+++ src/app/dashboard/onlineResources/page.tsx
@@ -14,29 +14,22 @@ const OnlineResourcesPage = () => {
       const folders = await getAllSubFolders(
         "1K6S8q9I9Qk0O0Dikf3sME9K_sHEwXSTs"
       );
-      const resources: { name: string; id: string; count: number }[] = [];
 
-      await Promise.all(
+      const folderData = await Promise.all(
         folders
           .filter(
             (folder) => folder.mimeType === "application/vnd.google-apps.folder"
           )
           .map(async (folder) => {
-            if (folder.id) {
-              const response = await getFolderResources(folder.id);
-              if (response) {
-                const resource = {
-                  name: folder.name,
-                  id: folder.id,
-                  count: response,
-                };
-                resources.push(resource);
-              }
-            }
-            return folder;
+            const response = await getFolderResources(folder.id);
+            return {
+              name: folder.name ?? "",
+              id: folder.id ?? "",
+              count: response ?? 0,
+            };
           })
       );
-      setResourceFolders(resources);
+      setResourceFolders(folderData);
     };
 
     fetchSubFolders();
@@ -49,19 +42,22 @@ const OnlineResourcesPage = () => {
           <p className="text-sm text-slate-500 mb-6">
             {resourceFolders.reduce((acc, cur) => {
               return cur.count + acc;
-            }, 0)}{" "}
-            {"resources"}
+            }, 0)}
+            {" resources"}
           </p>
         </div>
         <div>
           <ul className="grid grid-cols-2 gap-4">
-            {resourceFolders.map((folder) => (
-              <li key={folder.name}>
-                <div className="p-4 border-gray-200 border bg-white shadow-md rounded-md hover:bg-blue-100 transition duration-200 mb-10">
-                  <DisplayFolder resource={folder} />
-                </div>
-              </li>
-            ))}
+            {resourceFolders.map((folder) => {
+              if (folder.name && folder.id)
+                return (
+                  <li key={folder.id}>
+                    <div className="p-4 border-gray-200 border bg-white shadow-md rounded-md hover:bg-blue-100 transition duration-200 mb-10">
+                      <DisplayFolder resource={folder} />
+                    </div>
+                  </li>
+                );
+            })}
           </ul>
         </div>
       </div>
