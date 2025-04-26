fd9562c48b38c375da8dd115d9b80c2b7b2e6108
21hwang <130326162+21hwang@users.noreply.github.com>
2025-04-26 02:56:58 +0100

    Avasim/eng 105 security backend role checks (#92)
    * starting the ticket, don't really know how to test, made auth.ts and added to users/route.ts (see line 26)
    
    * some api routes protected
    
    * tutor delete request
    
    ---------
    
    Co-authored-by: ava-sim <avajsim@gmail.com>

--- src/app/api/requests/count/route.ts
+++ src/app/api/requests/count/route.ts
@@ -1,8 +1,16 @@
 import { NextResponse } from "next/server";
+import { requireUserWithRole } from "@/lib/auth";
 import { getRequestsCountController } from "./controller";
 
 // GET - Fetch all requests
 export async function GET(req: Request) {
+  try {
+    await requireUserWithRole(["Admin"]);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json({ message: "Unauthorized" }, { status: 403 });
+  }
+
   try {
     // grab the date ranges
     const { searchParams } = new URL(req.url);
